a4b28a4c14710efb1a113c2b674926e6d20a8782
enrique <santosenrique869@gmail.com>
2022-03-26 20:37:40 -0300

    alteração no js

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
 let hours = document.querySelector('.hours h1'),
     minutes = document.querySelector('.minutes h1'),
     seconds = document.querySelector('.seconds h1'),
-    ampm = document.querySelector('.box-clock span');
+    toogleAmPm = document.querySelector('.box-clock span'),
+    date = new Date();
 
-let date = new Date()
 
-function AmPm() {
-    time()
+const AmPm = () => {
     let atualDate = parseInt(hours.textContent + minutes.textContent + seconds.textContent)
 
-    if(atualDate == 000000 || atualDate <= 115999){
-        ampm.innerHTML = 'am'
-    }
-    else if(atualDate >= 120000 || atualDate <= 125999){
-        ampm.innerHTML = 'pm'
-    }
-}
-function padAdd(){
-    if (hours.textContent.length == 1 ) {
-        hours.textContent = '0' + hours.textContent 
-    }
-    if(minutes.textContent.length == 1){
-        minutes.textContent = '0' + minutes.textContent
-    }
-    if(seconds.textContent.length == 1){
-        seconds.textContent = '0' + seconds.textContent
-    }
+    if (atualDate >= 120000 && atualDate <= 235958) toogleAmPm.innerHTML = 'Pm';
+    else { toogleAmPm.innerHTML = 'Am' };
 }
 
-function time() {
+
+const time = () => {
     date = new Date()
-    hours.innerHTML = date.getHours()
+
+    hours.innerHTML = date.getHours();
     minutes.innerHTML = date.getMinutes()
     seconds.innerHTML = date.getSeconds()
-    padAdd()
+
+    if (hours.textContent < 10) hours.textContent = '0' + hours.textContent;
+    if (minutes.textContent < 10) minutes.textContent = '0' + minutes.textContent;
+    if (seconds.textContent < 10) seconds.textContent = '0' + seconds.textContent;
+    AmPm()
 }
 
-
-setInterval(time,1000) // 1000 ms == 1s
-setInterval(AmPm,59000) //59000 ms == 59min
-
+setInterval(time, 1000) // 1000 ms == 1s
