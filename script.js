af60f2cfd574ef13ef0cb224e93670f131de0f7b
enrique <santosenrique869@gmail.com>
2022-03-26 15:55:47 -0300

    Push clock files

---
+++ script.js
@@ -0,0 +1,29 @@
+let hours = document.querySelector('.hours h1'),
+    minutes = document.querySelector('.minutes h1'),
+    seconds = document.querySelector('.seconds h1');
+
+function padAdd(){
+    if (hours.textContent.length == 1 ) {
+        hours.textContent = '0' + seconds.textContent 
+    }
+    if(minutes.textContent.length == 1){
+        minutes.textContent = '0' + seconds.textContent
+    }
+    if(seconds.textContent.length == 1){
+        seconds.textContent = '0' + seconds.textContent
+    }
+}
+
+function time() {
+    let date = new Date()
+    
+    
+    hours.innerHTML = date.getHours()
+    minutes.innerHTML = date.getMinutes()
+    seconds.innerHTML = date.getSeconds()
+    padAdd()
+}
+
+
+setInterval(time,1000)
+
